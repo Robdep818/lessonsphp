9f9bbb7c5587e3a4bdaf81249221291fcbb65e3a
Robdep818 <sunsmilesatyou@yahoo.com>
2022-11-03 19:33:35 +0800

    Strings Data Types

---
+++ lessons.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Intro to php</h1>
+    <h2>10 primitive types</h2>
+    <h2>Scalar</h2>
+    <ul>
+        <li>String</li>
+        <li>integer</li>
+        <li>float</li>
+        <li>Boolean</li>
+    </ul>
+
+    <h2>Compounds</h2>
+    <li>Array</li>
+    <li>Object</li>
+    <li>Callable</li>
+    <li>Iterable</li>
+
+    <h2>Special</h2>
+    <ul>
+        <li>Resource</li>
+        <li>Null</li>
+    </ul>
+
+    <h2>Pseudo Types</h2>
+    <hr />
+ 
+    <h2>Strings</h2>
+    <h3>Double Quotes</h3>
+    <p>"This is a string data type in php</p>
+    <h1>Single Quotes</h1>
+    <p>'This is a string data stype in php</p>
+    <p>"SELECT person WHERE name- 'Rob' </p>
+
+    <?php 
+        /*incorrect
+        echo 'I'm learning to work strings in Php'; */
+    
+        //correct
+        echo 'I\'m learning how to work with strings in php';
+        ?>
+</body>
+</html>
